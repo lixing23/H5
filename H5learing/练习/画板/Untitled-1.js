63b5e3ee54e837a5bc27282bdc6883904f857480
lixing23 <2236170108@sdbi.edu.cn>
2023-12-25 21:16:56 +0800

    master

--- H5learing/练习/画板/Untitled-1.js
+++ H5learing/练习/画板/Untitled-1.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+
+(function () {
+    
+var canvas = document.getElementById("drawing");
+var context = canvas.getContext('2d');
+canvas.width = 900;
+canvas.height = 600;
+canvas.style.border = "1px solid #000";
+canvas.onmousedown = function(e){
+    var x = e.clientX - canvas.getBoundingClientRect().left;
+    var y = e.clientY - canvas.getBoundingClientRect().top;
+    context.beginPath();
+    context.moveTo(x,y);
+    canvas.onmousemove = function(event){
+        var x = event.clientX - canvas.getBoundingClientRect().left;
+        var y = event.clientY - canvas.getBoundingClientRect().top;
+        context.lineTo(x,y);
+        context.stroke();
+    };
+    canvas.onmouseup = function(event){
+        canvas.onmousemove = null;
+    };
+};
+}());
